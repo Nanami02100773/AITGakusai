3aa2483850574ddce19a9439617a137a66319a95
Nanami02100773 <uuiaai.ieo773@icloud.com>
2026-04-17 13:16:27 +0900

    微調整

---
+++ app/stage/components/StageTitle.jsx
@@ -0,0 +1,9 @@
+import "./StageTitle.css";
+
+export default function StageTitle() {
+  return (
+    <div className="Stage-Title">
+      ステージ情報
+    </div>
+  );
+}
--- app/stage/page.jsx
+++ app/stage/page.jsx
@@ -7,13 +7,14 @@ import NavigationBar from "./components/NavigationBar";
 import StageScheduleSection from "./components/StageScheduleSection";
 
 import "./global.css";
+import StageTitle from "./components/StageTitle";
 
 export default function App() {
   return (
     <div>
+      <StageTitle/>
       <NoticeSection />
       <NavigationBar />
-    
       <StageScheduleSection />
     </div>
   );
