6d391412217e642953b38a1d5d3da8a1589c43eb
Nanami02100773 <uuiaai.ieo773@icloud.com>
2026-05-30 21:26:59 +0900

    iii

--- app/question/components/FaqSection.jsx
+++ app/question/components/FaqSection.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import "./FaqSection.css";
 
@@ -12,39 +13,62 @@ const FaqSection = ({
 }) => {
   return (
     <div className="faq-item-wrapper">
+      {/* セクションヘッダー */}
       <button
         className="faq-item-header"
         onClick={() => toggleSection(sectionIndex)}
       >
         <div className="faq-left">
           <div className="faq-icon">
-            {/* 仮：線アイコン風 */}
             <div className="icon-line"></div>
           </div>
-          <span className="faq-title">{section?.title}</span>
+
+          <span className="faq-title">
+            {section?.title}
+          </span>
         </div>
 
-        <span className={`arrow ${isOpen ? "open" : ""}`}></span>
+        <div className="faq-toggle">
+          <span
+            className={`arrow ${
+              isOpen ? "open" : ""
+            }`}
+          ></span>
+        </div>
       </button>
 
+      {/* 開いた時 */}
       {isOpen && (
         <div className="faq-box-wrapper">
-          {section.items.map((pair, qIndex) => {
+          {section?.items?.map((pair, qIndex) => {
             const qKey = `${sectionIndex}-${qIndex}`;
             const isQOpen = openQuestions[qKey];
 
             return (
-              <div key={qKey} className="faq-pair">
+              <div
+                key={qKey}
+                className="faq-pair"
+              >
+                {/* 質問 */}
                 <button
                   className="faq-question"
-                  onClick={() => toggleQuestion(qKey)}
+                  onClick={() =>
+                    toggleQuestion(qKey)
+                  }
                 >
-                  Q {pair.q}
+                  <span className="q-circle">
+                    Q
+                  </span>
+
+                  <span className="question-text">
+                    {pair.q}
+                  </span>
                 </button>
 
+                {/* 回答 */}
                 {isQOpen && (
-                  <div className="faq-answer-content">
-                    A {pair.a}
+                  <div className="faq-answer-card">
+                    {pair.a}
                   </div>
                 )}
               </div>
