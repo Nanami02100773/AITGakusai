c1922e8ed025bcbbd6110a2a8c7a9aa832e5b3fa
Nanami02100773 <uuiaai.ieo773@icloud.com>
2026-06-10 13:17:28 +0900

    iii

--- app/ProjectStampRally/components/DetailTable.jsx
+++ app/ProjectStampRally/components/DetailTable.jsx
@@ -4,30 +4,32 @@ import "./DetailTable.css";
 function DetailTable() {
   const details = [
     {
-      icon: "📍",
+      icon: "/project/place.png",
+      iconClass: "place-icon",
       label: "場所",
       value: "工科展／楽市楽座／脱出ゲーム／MAKE",
     },
     {
-      icon: "🕒",
+      icon: "/project/clock.png",
+      iconClass: "clock-icon",
       label: "時間",
       value: "10:00～17:00",
     },
     {
-  icon: "⚠",
-  label: "注意事項",
-  value: [
-    "スタンプは1人1回までです",
-    "景品は数に限りがあります",
-    "総合案内所で交換してください",
-    "学祭と執行のスタンプラリーがあります",
-  ],
-}
+      icon: "/project/Precautions.png",
+      iconClass: "precautions-icon",
+      label: "注意事項",
+      value: [
+        "スタンプは1人1回までです",
+        "景品は数に限りがあります",
+        "総合案内所で交換してください",
+        "学祭と執行のスタンプラリーがあります",
+      ],
+    },
   ];
 
   return (
     <section>
-
       <div className="ProjectStampRally-section-wrapper">
         <div className="StampRally-section-title">
           詳細情報
@@ -35,40 +37,41 @@ function DetailTable() {
       </div>
 
       <div className="ProjectStampRally-detail-table">
-
         {details.map((item, index) => (
           <div
             className="ProjectStampRally-detail-row"
             key={index}
           >
-           <div className="ProjectStampRally-detail-label">
-  <span className="ProjectStampRally-detail-icon">
-    {item.icon}
-  </span>
+            <div className="ProjectStampRally-detail-label">
+              <span className="ProjectStampRally-detail-icon">
+                <img
+                  src={item.icon}
+                  alt={item.label}
+                  className={`ProjectStampRally-detail-icon-image ${item.iconClass}`}
+                />
+              </span>
 
-  <span className="ProjectStampRally-detail-text">
-    {item.label}
-  </span>
-</div>
+              <span className="ProjectStampRally-detail-text">
+                {item.label}
+              </span>
+            </div>
 
             <div className="ProjectStampRally-detail-content">
-            <div className="ProjectStampRally-detail-value">
-  {Array.isArray(item.value) ? (
-    <ul className="ProjectStampRally-note-list">
-      {item.value.map((note, i) => (
-        <li key={i}>{note}</li>
-      ))}
-    </ul>
-  ) : (
-    item.value
-  )}
-</div>
+              <div className="ProjectStampRally-detail-value">
+                {Array.isArray(item.value) ? (
+                  <ul className="ProjectStampRally-note-list">
+                    {item.value.map((note, i) => (
+                      <li key={i}>{note}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  item.value
+                )}
+              </div>
             </div>
           </div>
         ))}
-
       </div>
-
     </section>
   );
 }
