5b008278c2106fbfeebec0e81fbd11ffe8569e73
SatooRu65536 <ryuto65536@gmail.com>
2026-08-05 18:18:16 +0900

    feat: AITGuideIframeコンポーネントを追加し、StandbyScreenで使用

--- app/map/components/StandbyScreen.jsx
+++ app/map/components/StandbyScreen.jsx
@@ -1,31 +1,19 @@
 "use client";
 import React, { useState } from "react";
-import { useSearchParams } from 'next/navigation';
-import { useIframeParams } from '@/hooks/useIframeChildSearchParams';
+import AITGuideIframe from "./AITGuideIframe";
+import { Suspense } from "react";
 
 const App = () => {
-  const searchParams = useSearchParams();
-  // 読み込み時に持つ searchParams を iframe に渡す
-  const [url] = useState(`https://ait-guide.sysken.net/?${searchParams.toString()}`);
-
-  useIframeParams();
-
   return (
     <div
       style={{
         height: "100vh",
-        padding: "50px 0 70px"
+        padding: "50px 0 70px",
       }}
     >
-      <iframe
-        src={url}
-        // 位置情報の取得 と クリップボードの書き込み(URL共有機能のため) を許可
-        allow="geolocation; clipboard-write"
-        style={{
-          width: "100%",
-          height: "100%"
-        }}
-      ></iframe>
+      <Suspense>
+        <AITGuideIframe />
+      </Suspense>
     </div>
   );
 };
