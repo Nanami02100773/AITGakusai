5a40e5884654d1d46a75b22f8b639edbbdf2f6a7
Nanami02100773 <uuiaai.ieo773@icloud.com>
2026-09-07 11:10:58 +0900

    aaaaa

--- app/ProjectKyouyasai/components/DetailTable.jsx
+++ app/ProjectKyouyasai/components/DetailTable.jsx
@@ -7,23 +7,35 @@ function DetailTable() {
       icon: "/project/place.png",
       iconClass: "place-icon",
       label: "場所",
-      value: "AITプラザ",
+      value: "セントラル広場",
     },
     {
       icon: "/project/clock.png",
       iconClass: "clock-icon",
       label: "時間",
-      value: "10:00～17:00",
+      value: "15:50～20:00",
     },
     {
       icon: "/project/Precautions.png",
       iconClass: "precautions-icon",
       label: "注意事項",
       value: [
-        "スタンプは1人1回までです",
-        "景品は数に限りがあります",
-        "総合案内所で交換してください",
-        "学祭と執行のスタンプラリーがあります",
+        "リフト、ダイブ、サーフ、モッシュ等の危険行為",
+        "開場時間前、早朝から並ぶ行為",
+        "他のお客様に迷惑となる行為",
+        "ステージ設備を壊しかねない行為",
+        "ベンチの上に立つ行為",
+        "酒気を帯びた状態での入場",
+        "物を投げる行為",
+        "ステージの妨害となる行為",
+        "モラルに反する発言や行動",
+        "撮影を禁止としているパフォーマーの撮影",
+        "脚立、目線よりも高い三脚を使用した撮影",
+        "受付以外からの入場",
+        "その他、運営側の判断で、他のお客様、イベント側、運営側に迷惑をかける行為",
+        "雨天時、演者発表中は傘さしが原則禁止となります。（ステージ観覧エリアに限る）",
+        "そのため、カッパの持参をお願いします。",
+        "安全のため、トラロープには触らずに少し離れてご観覧ください。",
       ],
     },
   ];
@@ -59,11 +71,24 @@ function DetailTable() {
             <div className="kyoyasai-detail-content">
               <div className="kyoyasai-detail-value">
                 {Array.isArray(item.value) ? (
-                  <ul className="kyoyasai-note-list">
-                    {item.value.map((note, i) => (
-                      <li key={i}>{note}</li>
-                    ))}
-                  </ul>
+                  <>
+                    <ul className="kyoyasai-note-list">
+                      {item.value.map((note, i) => (
+                        <li key={i}>{note}</li>
+                      ))}
+                    </ul>
+
+                    <div className="kyoyasai-warning">
+                      <p>
+                        ※上記の事項が守られないときは、ライブを途中で中断するか、
+                        会場からご退場していただく場合がありますので、予めご了承ください。
+                      </p>
+
+                      <p>
+                        ※時間外でのトラブルに関しましては大学祭実行委員会は一切責任を負いません。
+                      </p>
+                    </div>
+                  </>
                 ) : (
                   item.value
                 )}
